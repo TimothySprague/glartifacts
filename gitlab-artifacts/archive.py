78dc6373809b7e7416586d7370b5442cdbd638f0
Mike Haboustak <mhaboustak@haboustak.local>
2018-03-17 06:44:01 -0400

    [archive] Refactor archive queries to reduce duplication
    The lastgood query is no longer duplicated between the two current
    strategies. Also added some descriptions around each sql block.

--- gitlab-artifacts/archive.py
+++ gitlab-artifacts/archive.py
@@ -17,15 +17,15 @@ class ArchiveStrategy(Enum):
 
 def get_archive_strategy_query(strategy):
     if strategy == ArchiveStrategy.LASTGOOD_BUILD:
-        return sql_lastgood_build
+        return sql_lastgood.format(sql_good_build)
     elif strategy == ArchiveStrategy.LASTGOOD_PIPELINE:
-        return sql_lastgood_pipeline
+        return sql_lastgood.format(sql_good_pipeline)
 
     raise Exception("Strategy {} not implemented".format(strategy.name))
 
 def list_archive_artifacts(db, project_id, strategy=ArchiveStrategy.LASTGOOD_BUILD):
     strategy_query = get_archive_strategy_query(strategy)
-    action_query = sql_artifact_list.format(sql_artifacts)
+    action_query = sql_artifact_list.format(sql_identify_artifacts)
     sql = strategy_query + action_query
     log.debug("Running %s archive query:\n  %s", strategy.name, indent(sql))
 
@@ -36,7 +36,7 @@ def list_archive_artifacts(db, project_id, strategy=ArchiveStrategy.LASTGOOD_BUI
 
 def archive_artifacts(db, project_ids, strategy=ArchiveStrategy.LASTGOOD_BUILD):
     strategy_query = get_archive_strategy_query(strategy)
-    action_query = sql_artifact_expire.format(sql_artifacts)
+    action_query = sql_artifact_expire.format(sql_identify_artifacts)
 
     sql = strategy_query + action_query
     log.debug("Running %s archive query:\n  %s", strategy.name, indent(sql))
@@ -44,25 +44,8 @@ def archive_artifacts(db, project_ids, strategy=ArchiveStrategy.LASTGOOD_BUILD):
     cur = db.cursor()
     cur.execute(sql, dict(project_id=tuple(project_ids)))
 
-# Archive artifacts older than the most recent successful pipeline
-sql_lastgood_pipeline = """
-with lastgood as (
-    select b.project_id, b.name,
-            max(p.created_at) as pipeline_date, 
-	    max(b.created_at) as build_date
-    from ci_builds as b
-    join ci_stages as s on s.id=b.stage_id
-    join ci_pipelines as p on p.id=s.pipeline_id
-    where p.status='success' and
-	    p.tag=false and
-	    b.artifacts_expire_at is null and
-            b.project_id in %(project_id)s
-    group by b.name, b.project_id
-)
-"""
-
-# Archive artifacts older than the most recent successful job
-sql_lastgood_build = """
+# Find the date of the most recent good pipeline and build
+sql_lastgood = """
 with lastgood as (
     select b.project_id, b.name,
             max(p.created_at) as pipeline_date, 
@@ -70,14 +53,36 @@ with lastgood as (
     from ci_builds as b
     join ci_stages as s on s.id=b.stage_id
     join ci_pipelines as p on p.id=s.pipeline_id
-    where (b.status='success' or b.allow_failure) and 
-            b.tag=false and
-            b.artifacts_expire_at is null and
-            b.project_id in %(project_id)s
+    where b.project_id in %(project_id)s and 
+        {}
     group by b.name, b.project_id
 )
 """
 
+# Criteria that define "good" for Build or Pipeline
+sql_good_build = "(b.status='success' or b.allow_failure)"
+sql_good_pipeline = "(p.status='success')"
+
+# Identifies old artifacts based on a lastgood strategy
+# "old" is defined as:
+#    Older than the lastgood build or pipeline
+#    Not tagged
+#    Not already expired
+#    Has artifacts file_type=1 (zip)
+sql_identify_artifacts = """
+from lastgood
+join ci_builds as b on b.project_id=lastgood.project_id and b.name=lastgood.name
+join ci_stages as s on s.id=b.stage_id
+join ci_pipelines as p on p.id=s.pipeline_id
+join ci_job_artifacts as a on a.job_id=b.id
+where b.created_at<lastgood.build_date
+    and p.created_at<lastgood.pipeline_date
+    and b.tag = false
+    and b.artifacts_expire_at is null
+    and a.file_type = 1
+"""
+
+# Wrapper that lists identified artifacts
 sql_artifact_list = """
 select a.size, b.name, b.status, b.tag,
     p.created_at as scheduled_at, b.created_at as built_at,
@@ -85,22 +90,12 @@ select a.size, b.name, b.status, b.tag,
 {}
 """
 
+# Wrapper that sets artifact expiration on identified artifacts
 sql_artifact_expire = """
-update ci_builds as t
+update ci_builds as target
 set artifacts_expire_at=(now() at time zone 'utc')
 {}
-    and t.id=b.id
+    and target.id=b.id
 """
 
-sql_artifacts = """
-from lastgood
-join ci_builds as b on b.project_id=lastgood.project_id and b.name=lastgood.name
-join ci_stages as s on s.id=b.stage_id
-join ci_pipelines as p on p.id=s.pipeline_id
-join ci_job_artifacts as a on a.job_id=b.id
-where b.created_at<lastgood.build_date
-    and p.created_at<=lastgood.pipeline_date
-    and b.artifacts_expire_at is null
-    and b.tag = false
-    and a.file_type = 1
-"""
+
