4063304778a9fcf787af33232d59bc73768d9e58
Mike Haboustak <haboustak@gmail.com>
2018-10-16 03:51:39 -0400

    Add Gitaly.FindAllBranchNames implementation
    The path to the Gitaly socket will need to be configurable. It's
    possible for a GitLab instance to have multiple Gitaly instances. We may
    also need to connect to Gitaly over tcp and support GitLab's pre-shared
    key authentication.

---
+++ glartifacts/gitaly/client.py
@@ -0,0 +1,39 @@
+import grpc
+
+from .proto import ref_pb2, ref_pb2_grpc
+from .proto import shared_pb2
+
+GITALY_ADDR = 'unix:/var/opt/gitlab/gitaly/gitaly.socket'
+
+class GitalyClient(object):
+    def __init__(self, addr=GITALY_ADDR):
+        self.addr = addr
+        self._channel = None
+        self._ref_service = None
+
+    def __enter__(self):
+        self._channel = grpc.insecure_channel(self.addr)
+        self._ref_service = ref_pb2_grpc.RefServiceStub(self._channel)
+
+        return self
+
+    def __exit__(self, *args):
+        self._channel.close()
+
+    def get_branches(self, project):
+        repository = shared_pb2.Repository(
+            storage_name=project.storage,
+            relative_path=project.disk_path,
+            gl_repository=project.gl_repository,
+            )
+        request = ref_pb2.FindAllBranchNamesRequest(
+            repository=repository
+            )
+
+        response = list(self._ref_service.FindAllBranchNames(request))
+        assert len(response) == 1 # Shouldn't this be Unary?
+
+        branches = [ref.decode('utf-8').split('/')[-1] for ref in response[0].names]
+        assert len(branches) > 0 # Safety check for failed requests
+
+        return branches
