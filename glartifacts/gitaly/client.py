b0c73d5f68bd543d860e0061b3cd3083b850dfc2
Mike Haboustak <haboustak@gmail.com>
2018-12-04 06:00:01 -0500

    Add support for CommitService.TreeEntry
    The CommitService.TreeEntry procedure retrieves a file from git at a
    specific revision (commit). This will be used to grab .gitlab-ci.yml.

--- glartifacts/gitaly/client.py
+++ glartifacts/gitaly/client.py
@@ -1,6 +1,10 @@
 import grpc
 
-from .proto import ref_pb2, ref_pb2_grpc
+from .proto import (
+    ref_pb2, ref_pb2_grpc,
+    commit_pb2, commit_pb2_grpc,
+    blob_pb2, blob_pb2_grpc,
+    )
 from .proto import shared_pb2
 
 GITALY_ADDR = 'unix:/var/opt/gitlab/gitaly/gitaly.socket'
@@ -9,28 +13,35 @@ class GitalyClient(object):
     def __init__(self, addr=GITALY_ADDR):
         self.addr = addr
         self._channel = None
-        self._ref_service = None
+        self._refsvc = None
+        self._commitsvc = None
+        self._blobsvc = None
 
     def __enter__(self):
         self._channel = grpc.insecure_channel(self.addr)
-        self._ref_service = ref_pb2_grpc.RefServiceStub(self._channel)
+        self._refsvc = ref_pb2_grpc.RefServiceStub(self._channel)
+        self._commitsvc = commit_pb2_grpc.CommitServiceStub(self._channel)
+        self._blobsvc = blob_pb2_grpc.BlobServiceStub(self._channel)
 
         return self
 
     def __exit__(self, *args):
         self._channel.close()
 
-    def get_branches(self, project):
-        repository = shared_pb2.Repository(
+    def _project_repo(self, project):
+        return shared_pb2.Repository(
             storage_name=project.storage,
             relative_path=project.disk_path,
             gl_repository=project.gl_repository,
             )
+
+    def get_branches(self, project):
+        repository = self._project_repo(project)
         request = ref_pb2.FindAllBranchesRequest(
             repository=repository
             )
 
-        response = list(self._ref_service.FindAllBranches(request))
+        response = list(self._refsvc.FindAllBranches(request))
         assert len(response) == 1 # Shouldn't this be Unary?
 
         branches = []
@@ -43,3 +54,22 @@ class GitalyClient(object):
         assert len(branches) > 0 # Safety check for failed requests
 
         return branches
+
+    def get_tree_entry(self, project, commit_id, path):
+        repository = self._project_repo(project)
+
+        request = commit_pb2.TreeEntryRequest(
+            repository=repository,
+            revision=commit_id.encode('utf-8'),
+            path=path.encode('utf-8'),
+            limit = 0,
+            )
+        response = list(self._commitsvc.TreeEntry(request))
+        assert len(response) == 1 # Shouldn't this be Unary?
+
+        entry = response[0]
+        return (
+            entry.oid,
+            entry.size,
+            entry.data,
+            )
