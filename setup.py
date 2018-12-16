fa8d665de414d3dbbc923fc3b81ec8133d51660c
Mike Haboustak <haboustak@gmail.com>
2018-12-16 10:37:12 -0500

    Update installer to support PyPI

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 import glartifacts.version
 import setuptools
 
+from os import path
+
+def long_description():
+    readme = path.join(path.dirname(__file__), 'README.md')
+    with open(readme, 'rt') as f:
+        return f.read()
+
 setuptools.setup(
     name='glartifacts',
     version=glartifacts.version.__version__,
     description='Tools for managing GitLab CI build artifacts',
+    long_description = long_description(),
+    long_description_content_type = 'text/markdown',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+	'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities',
+
+    ],
     license='MIT',
     keywords='GitLab',
     author='Mike Haboustak',
